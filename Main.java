2a17eeaf33840acdbd3703bcd4538151cc4bee35
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-13 20:38:45 -0500

    Iniciazcion de pruba clase empleado y persona

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Persona persona = new Persona("Messi", 38);
+        persona.mostrarDetalles();
+        Empleado empleado = new Empleado("Cristiano", 40, "YouTube");
+        empleado.mostrarDetalles();
     }
 }
